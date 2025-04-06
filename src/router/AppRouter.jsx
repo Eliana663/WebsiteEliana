1918ea1e548bb61c91ebb369b4693d6eafc68ce5
Eliana663 <elianadtorrest@gmail.com>
2025-04-06 20:42:39 +0200

    Cambio background CSS

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import { Footer }  from '../Footer';
 import { Routes, Route } from 'react-router-dom'
 import Layout from "../components/Layout";
 import {About, Timeline, Projects, Libraries, Home } from "../pages"
-import background from "../assets/img/fondo1.jpg"
+
 
 
 
@@ -13,7 +13,8 @@ function AppRouter() {
   return (
     
     <>
-    <div id="contenedorPrincipal" style={{ backgroundImage: `url(${background})` }} className="Container">
+    <div id="contenedorPrincipal" >
+    
     <div >
        <Routes>
         <Route path="/" element={<Layout />}>
@@ -23,18 +24,18 @@ function AppRouter() {
            <Route path="Projects" element={<Projects />}/>
            <Route path="Libraries" element={<Libraries />}/>
            <Route path="*" element={<Home />}/>
-           </Route>
+        </Route>
        
   
       </Routes>
+        <div>
+           <Footer>
+           </Footer>
+        </div>
+          
       </div>
-
-
-      <div>
-        <Footer>
-        </Footer>
     </div>
-    </div>
+     
     
     </>
   );
