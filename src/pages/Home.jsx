614878901c13ef7c26204b5f630fe498dc2c5036
Eliana663 <elianadtorrest@gmail.com>
2025-05-20 22:31:23 +0200

    paragraph modification

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,9 +10,9 @@ export const Home = () => {
   
     <div id="textoHome">
       <h1 id="eliana" > Eliana Torres </h1>
-      <p> Interesada en obtener experiencia en el mundo tecnológico</p>
+      <p> Interesada en obtener experiencia en Tecnología</p>
       <p> Apasionada por el mundo tech y el desarrollo de software, actualmente especializándome en Java, Kotlin y React </p>
-      <p> Aquí puedes conocer mis proyectos, mi cammino en el mundo del desarrollo y cómo puedo aportar en el área tecnológica</p>
+      <p> Aquí puedes conocer mis proyectos, mi camino en el mundo del desarrollo y cómo puedo aportar en el área tecnológica</p>
       <h5 id="pensamiento">"Las que conducen y arrastran al mundo no son las máquinas, sino las ideas" (Victor Hugo) </h5>
     </div>
    
