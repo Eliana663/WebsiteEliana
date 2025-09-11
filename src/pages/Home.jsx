6590a4bcc0bd1f87b98dafb92175f0f64d610598
Eliana663 <elianadtorrest@gmail.com>
2025-09-11 17:06:51 +0200

    Add images and modifying profile font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,8 @@ export const Home = () => {
     <div id="textoHome">
       <h1 id="eliana" > Eliana Torres </h1>
       <p> Interesada en obtener experiencia en el área de TI</p>
-      <p> ✨ Apasionada por la tecnología y el desarrollo de software, actualmente me especializo en Java, Kotlin y React. Mi trayectoria en gestión y administración de procesos me ha dado una visión estratégica y estructurada, que ahora aplico al mundo IT. 🚀 </p>
-      <p> Me motiva crear soluciones útiles, claras y con impacto real. Si quieres conocer cómo combino la organización y eficiencia de la gestión con la creatividad y lógica del desarrollo, te invito a explorar mis proyectos. 💡👩‍💻</p>
+      <p> ✨ Aprendiendo Java, Kotlin y React, combinando organización y creatividad para crear soluciones útiles. 💻 </p>
+      <p> Te invito a explorar mis proyectos y a conocerme un poco mas</p>
       <h5 id="pensamiento">"Las que conducen y arrastran al mundo no son las máquinas, sino las ideas" (Victor Hugo) </h5>
     </div>
    
--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -5,7 +5,7 @@ export const Timeline = () => {
     <div className="container my-5">
       <h1 className="titulos text-center mb-4">Timeline</h1>
       <p id="timelineParrafo" className="text-center mb-5">
-        A continuación te presento una cronología de todos mis estudios y empleos:
+        A continuación, se presenta una cronología detallada de mi formación académica y experiencia profesional:
       </p>
       <Timetable />
     </div>
