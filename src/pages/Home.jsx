da6bb8d25561dea8137598811a65812d3fdfcd09
Eliana663 <elianadtorrest@gmail.com>
2025-09-11 13:04:23 +0200

    Improvement in responsive screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,9 +10,9 @@ export const Home = () => {
   
     <div id="textoHome">
       <h1 id="eliana" > Eliana Torres </h1>
-      <p> Interesada en obtener experiencia en Tecnología</p>
-      <p> Apasionada por el mundo tech y el desarrollo de software, actualmente especializándome en Java, Kotlin y React </p>
-      <p> Aquí puedes conocer mis proyectos, mi camino en el mundo del desarrollo y cómo puedo aportar en el área tecnológica</p>
+      <p> Interesada en obtener experiencia en el área de TI</p>
+      <p> ✨ Apasionada por la tecnología y el desarrollo de software, actualmente me especializo en Java, Kotlin y React. Mi trayectoria en gestión y administración de procesos me ha dado una visión estratégica y estructurada, que ahora aplico al mundo IT. 🚀 </p>
+      <p> Me motiva crear soluciones útiles, claras y con impacto real. Si quieres conocer cómo combino la organización y eficiencia de la gestión con la creatividad y lógica del desarrollo, te invito a explorar mis proyectos. 💡👩‍💻</p>
       <h5 id="pensamiento">"Las que conducen y arrastran al mundo no son las máquinas, sino las ideas" (Victor Hugo) </h5>
     </div>
    
